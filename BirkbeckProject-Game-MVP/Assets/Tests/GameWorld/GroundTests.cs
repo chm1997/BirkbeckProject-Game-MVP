4f4dfedca2c90f49178d77f21f2380b970cf04aa
chm1997 <calynhughesmcinnes@gmai.com>
2023-06-13 11:25:01 +0100

    refactor GroundScriptTests to have setup and teardown

--- BirkbeckProject-Game-MVP/Assets/Tests/GameWorld/GroundTests.cs
+++ BirkbeckProject-Game-MVP/Assets/Tests/GameWorld/GroundTests.cs
@@ -9,82 +9,72 @@ public class GroundTests
     GameObject groundPrefab = Resources.Load<GameObject>("Ground");
     GameObject playerPrefab = Resources.Load<GameObject>("Player");
 
+    GameObject player;
+    GameObject ground;
+
+    [SetUp]
+    public void GroundTest_Setup()
+    {
+        Debug.Log("set up");
+        player = GameObject.Instantiate(playerPrefab, new Vector3(0, 5, 0), Quaternion.identity);
+        ground = GameObject.Instantiate(groundPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    [TearDown]
+    public void GroundTest_TearDown()
+    {
+        Debug.Log("tear down");
+        UnityEngine.Object.Destroy(player);
+        UnityEngine.Object.Destroy(ground);
+    }
+
+
     [UnityTest]
     public IEnumerator GroundTest_Stationary()
     {
-        GameObject playerStationary = GameObject.Instantiate(playerPrefab, new Vector3(0, 5, 0), Quaternion.identity);
-        GameObject groundStationary = GameObject.Instantiate(groundPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        yield return new WaitForSeconds(0.5f);
 
-        yield return new WaitForSeconds(0.1f);
-
-        Assert.AreEqual(0.0f, groundStationary.transform.position.x);
-        Assert.AreEqual(0.0f, groundStationary.transform.position.y);
-        
-        UnityEngine.Object.Destroy(playerStationary);
-        UnityEngine.Object.Destroy(groundStationary);
+        Assert.AreEqual(0.0f, ground.transform.position.x);
+        Assert.AreEqual(0.0f, ground.transform.position.y);
     }
-
+    
     [UnityTest]
     public IEnumerator GroundTest_HorizontalMovementLeft()
     {
-        GameObject playerHorizontalMovementLeft = GameObject.Instantiate(playerPrefab, new Vector3(0, 5, 0), Quaternion.identity);
-        GameObject groundHorizontalMovementLeft = GameObject.Instantiate(groundPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-
-        playerHorizontalMovementLeft.transform.position = new Vector2(-5, 0);
+        player.transform.position = new Vector2(-5, 0);
 
         yield return new WaitForSeconds(1.0f);
 
-        Assert.AreEqual(-5.0f, groundHorizontalMovementLeft.transform.position.x);
-
-        UnityEngine.Object.Destroy(playerHorizontalMovementLeft);
-        UnityEngine.Object.Destroy(groundHorizontalMovementLeft);
+        Assert.AreEqual(-5.0f, ground.transform.position.x);
     }
-
+    
     [UnityTest]
     public IEnumerator GroundTest_HorizontalMovementRight()
     {
-        GameObject playerHorizontalMovementRight = GameObject.Instantiate(playerPrefab, new Vector3(0, 5, 0), Quaternion.identity);
-        GameObject groundHorizontalMovementRight = GameObject.Instantiate(groundPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-
-        playerHorizontalMovementRight.transform.position = new Vector2(5, 5);
+        player.transform.position = new Vector2(5, 5);
 
         yield return new WaitForSeconds(1.0f);
 
-        Assert.AreEqual(5.0f, groundHorizontalMovementRight.transform.position.x);
-
-        UnityEngine.Object.Destroy(playerHorizontalMovementRight);
-        UnityEngine.Object.Destroy(groundHorizontalMovementRight);
+        Assert.AreEqual(5.0f, ground.transform.position.x);
     }
 
     [UnityTest]
     public IEnumerator GroundTest_VerticalMovementUp()
     {
-        GameObject playerVerticalMovementUp = GameObject.Instantiate(playerPrefab, new Vector3(0, 5, 0), Quaternion.identity);
-        GameObject groundVerticalMovementUp = GameObject.Instantiate(groundPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-
-        playerVerticalMovementUp.transform.position = new Vector2(0, 5);
+        player.transform.position = new Vector2(0, 5);
 
         yield return new WaitForSeconds(0.1f);
 
-        Assert.AreEqual(0.0f, groundVerticalMovementUp.transform.position.y);
-
-        UnityEngine.Object.Destroy(playerVerticalMovementUp);
-        UnityEngine.Object.Destroy(groundVerticalMovementUp);
+        Assert.AreEqual(0.0f, ground.transform.position.y);
     }
 
     [UnityTest]
     public IEnumerator GroundTest_VerticalMovementDown()
     {
-        GameObject playerVerticalMovementDown = GameObject.Instantiate(playerPrefab, new Vector3(0, 5, 0), Quaternion.identity);
-        GameObject groundVerticalMovementDown = GameObject.Instantiate(groundPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-
-        playerVerticalMovementDown.transform.position = new Vector2(0, -5);
+        player.transform.position = new Vector2(0, -5);
 
         yield return new WaitForSeconds(0.1f);
 
-        Assert.AreEqual(0.0f, groundVerticalMovementDown.transform.position.y);
-
-        UnityEngine.Object.Destroy(groundVerticalMovementDown);
-        UnityEngine.Object.Destroy(groundVerticalMovementDown);
+        Assert.AreEqual(0.0f, ground.transform.position.y);
     }
 }
