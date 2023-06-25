bf265c4404bae42bcc45004d9a741c36fc476423
chm1997 <calynhughesmcinnes@gmai.com>
2023-06-25 19:33:15 +0100

    refactor MenuAndGamOverScreens folder scripts for code quality, including docstrings and internal variables

--- BirkbeckProject-Game-MVP/Assets/MenuAndGameOverScreens/RestartButton.cs
+++ BirkbeckProject-Game-MVP/Assets/MenuAndGameOverScreens/RestartButton.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 
 public class RestartButton : MonoBehaviour
 {
+    /// <summary>
+    /// This class loads the main game scene when associated button object is pressed
+    /// </summary>
+    
     public void OnPressed()
     {
+        //This method is called by the scene when the button object is pressed, loading the desert scene
         SceneManager.LoadScene("DesertScene");
     }
 }
