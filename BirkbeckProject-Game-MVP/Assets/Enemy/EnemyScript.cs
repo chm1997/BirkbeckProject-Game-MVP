9502c614f64c79e697209e56498336c17f201296
chm1997 <calynhughesmcinnes@gmai.com>
2023-06-27 16:30:46 +0100

    working updates temp for merge main

--- BirkbeckProject-Game-MVP/Assets/Enemy/EnemyScript.cs
+++ BirkbeckProject-Game-MVP/Assets/Enemy/EnemyScript.cs
@@ -4,8 +4,15 @@ public class EnemyScript : MonoBehaviour, IDamagingObject, IEnemy
 {
     [SerializeField]
     private EnemyDataScriptableObject enemyData;
+
+    private GameObject player;
+
+    public Vector2 targetVector;
+
     public bool isDamaging { get; set; }
     public int damageValue { get; set; }
+
+    private bool AboveChangePoint;
 
     private void Start()
     {
@@ -13,14 +20,30 @@ public class EnemyScript : MonoBehaviour, IDamagingObject, IEnemy
         damageValue = 1;
 
         enemyData.IncreaseEnemyCount();
+
+        AboveChangePoint = false;
+
+        player = GameObject.FindWithTag("Player");
     }
 
     private void Update()
     {
+        targetVector = player.transform.position;
         EnemyMovement();
     }
 
     private void EnemyMovement()
+    {
+        if (enemyData.GetEnemyCount() >= enemyData.GetBehaviourChangePoint()) GroupMovement();
+        else SingularMovement();
+    }
+
+    private void SingularMovement()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetVector, 3f * Time.deltaTime);
+    }
+
+    private void GroupMovement()
     {
 
     }
